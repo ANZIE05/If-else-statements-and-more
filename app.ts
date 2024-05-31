8d9d464116e3061fd028d2041804b2b6003ff20b
ANZIE05 <161513339+ANZIE05@users.noreply.github.com>
2024-05-31 20:36:25 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,206 @@
+// HOME WORK PRACTICE (IF/ELSE)
+
+// let doShopping = true;
+//      if(doShopping){
+// console.log("The shop is open now!");
+// } else {
+//     console.log("Sorry! The shop is close.")
+// }
+
+// let doShopping = false;
+//      if(doShopping){
+// console.log("The shop is open now!");
+// } else {
+//     console.log("Sorry! The shop is close.")
+// }
+// __________________________________________________________________________________________
+
+// HOME WORK PRACTICE (IF/ELSE IF/ELSE)
+
+// let makeBiryani = "chicken";
+// if(makeBiryani === "chicken"){
+//     console.log("Chicken is not available");
+
+// }else if(makeBiryani === "beef"){
+//     console.log("beef is also not available")
+
+// }else{
+//     console.log("no problem, food panda hai naa!")
+// }
+// // __________________________________________________________
+// let makeBiryani = "beef";
+// if(makeBiryani === "chicken"){
+//     console.log("Chicken is not available");
+
+// }else if(makeBiryani === "beef"){
+//     console.log("beef is also not available")
+
+// }else{
+//     console.log("no problem, food panda hai naa!")
+// }
+// // ____________________________________________________________
+// let makeBiryani;
+// if(makeBiryani === "chicken"){
+//     console.log("Chicken is not available");
+
+// }else if(makeBiryani === "beef"){
+//     console.log("beef is also not available")
+
+// }else{
+//     console.log("no problem, food panda hai naa!")
+// }
+// _______________________________________________________________
+// HOME WORK PRACTICE (CONDITIONAL TERNARY OPERATORS)
+
+// let doPrayer = true;
+// let time = doPrayer? "prayer" : "qaza";
+// console.log(`You should do ${time}`);
+
+// let doPrayer = false;
+// let time = doPrayer? "prayer" : "qaza"
+// console.log(`You should do ${time}`);
+// _______________________________________________________________
+// HOME WORK PRACTICE (SWITCH STATEMENTS)
+
+// let spentHolidays = "northernAreas";
+// switch (spentHolidays){
+
+//     case "dubai" : console.log("Too much expensive, not in a budget :-/")
+// break;
+
+// case "northernAreas" : console.log("Too much negative temperature, it's -20 :-|")
+// break;
+
+// default : console.log("Spend all holidays in karachi :-(")
+// }
+// ______________________________________________________
+// let spentHolidays = "dubai";
+// switch (spentHolidays){
+
+//     case "dubai" : console.log("Too much expensive, not in a budget :-/")
+// break;
+
+// case "northernAreas" : console.log("Too much temperature, it's -20 :-|")
+// break;
+
+// default : console.log("Spend all holidays in karachi :-(")
+// }
+// ______________________________________________________
+// let spentHolidays;
+// switch (spentHolidays){
+
+//     case "dubai" : console.log("Too much expensive, not in a budget :-/")
+// break;
+
+// case "northernAreas" : console.log("Too much negative temperature, it's -20 :-|")
+// break;
+
+// default : console.log("Spend all holidays in karachi :-(")
+// }
+// ______________________________________________________
+// HOME WORK PRACTICE (SELF-CHECK QUIZ)
+
+// let answer = "notcorrect";
+// if(answer === "correct"){
+//     console.log("you are right");
+// }else{
+//     console.log("your ans is not correct");
+// }
+// ______________________________________________________
+// HOME WORK PRACTICE (NUMBER GAME)
+
+// let num1 = 5
+// let num2 = 10
+// if(num1 < num2){
+
+//     console.log("your ans is correct");
+// }else if(num1 > num2){
+//     console.log("Your ans is incorrect");
+
+// }else{
+//     console.log("let's forgeted");
+// }
+// _________________________________________________
+// let num1 = 5
+// let num2 = 10
+// if(num1 < num2){
+//     console.log("your ans is incorrect");
+
+// }else if(num1 > num2){
+//     console.log("Your ans is correct");
+
+// }else{
+//     console.log("let's forgeted");
+// }
+// __________________________________________________
+
+// let num1
+// let num2
+// if(num1 < num2){
+//     console.log("your ans is incorrect");
+
+// }else if(num1 > num2){
+//     console.log("Your ans is correct");
+
+// }else{
+//     console.log("let's forgeted");
+// }
+// ___________________________________________
+// HOME WORK PRACTICE (CHECKER GAME)
+
+// let isSize = "small";
+// if (isSize === "small" || isSize === "medium") {
+//     console.log(`${isSize} size is perfect`)
+// }else{
+//     console.log(`${isSize} is not your size`)
+// }
+// ____________________________________________
+// let isSize = "small";
+// if (isSize === "medium" || isSize === "large") {
+//     console.log(`${isSize} size is perfect`)
+// }else{
+//     console.log(`${isSize} is not your size`)
+// }
+// _____________________________________________
+// HOME WORK PRACTICE (LUDO GAME)
+
+// let person1 = "blue";
+// let person2 = "green";
+// if(person1 === person2){
+//     console.log("it's a tie");
+// }
+// else if((person1 === "blue" && person2 === "green") || (person1 === "green" && person2 === "red") || 
+// (person1 === "red" && person2 === "blue")){
+//     console.log("person1 wins");
+// }
+// else{
+//     console.log("person2 wins");
+// }
+// ___________________________________________________
+// let person1 = "blue";
+// let person2 = "green";
+// if(person1 === person2){
+//     console.log("it's a tie");
+// }
+// else if((person1 === "blue" && person2 === "red") || (person1 === "green" && person2 === "red") || 
+// (person1 === "red" && person2 === "blue")){
+//     console.log("person1 wins");
+// }
+// else{
+//     console.log("person2 wins");
+// }
+// ____________________________________________________
+// let person1;
+// let person2;
+// if(person1 === person2){
+//     console.log("it's a tie");
+// }
+// else if((person1 === "blue" && person2 === "red") || (person1 === "green" && person2 === "red") || 
+// (person1 === "red" && person2 === "blue")){
+//     console.log("person1 wins");
+// }
+// else{
+//     console.log("person2 wins");
+// }
+// ___________________________________________________
+
